c00cc15cc2495228eac7c68046df0d8b11228bd5
Peter Decuyper <github@serial-graphics.be>
2015-02-08 09:09:30 +0100

    Added documentation link.

--- codeclimate.sh
+++ codeclimate.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+# @see http://adamwathan.me/2014/12/28/test-coverage-code-climate-travis-ci
+
 php vendor/bin/test-reporter --stdout > codeclimate.json
 curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
